9400352561d81ac9f772b329c256b56e82421fbf
ALAM-97 <alexamara97@gmail.com>
2021-08-25 17:58:42 +0200

    Snack Completed

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,5 +1,14 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
-var userNumber = prompt("inserisci un numero");
 
 var numbers = [];
-push.numbers('userNumber');
+
+for (i = 0; i < 6; i++) {
+    var userNumber = parseInt(prompt("inserisci un numero"));
+    if (userNumber % 2 == 1) {
+        numbers.push(userNumber);
+    } else {
+
+    }
+}
+
+document.getElementById('dispari').innerHTML = numbers;
