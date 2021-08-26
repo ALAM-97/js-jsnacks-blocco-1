3779d2fd91b1fa4dbe58fe76e667b6391746f174
ALAM-97 <alexamara97@gmail.com>
2021-08-26 10:39:43 +0200

    FIX - JS

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,13 +1,11 @@
-//Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
-
+// 1. Crea un array vuoto.
 var numbers = [];
-
+// 2. Chiedi per 6 volte all’utente di inserire un numero.
 for (i = 0; i < 6; i++) {
     var userNumber = parseInt(prompt("inserisci un numero"));
+    // 2a. Se è dispari inseriscilo nell’array.
     if (userNumber % 2 == 1) {
         numbers.push(userNumber);
-    } else {
-
     }
 }
 
